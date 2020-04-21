d06b7583d707f539f1c9e5526d33dd76b0e315ee
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-04-21 08:42:58 +0300

    Add tests for schemaloader

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,16 @@
 import unittest
-from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
-from aiohttp import web, FormData
-from metadata_backend.server import init, main
-from unittest import mock
+import xmlschema
+
 from pathlib import Path
+from metadata_backend.schema_load import SchemaLoader
 from metadata_backend.logger import LOG
 
+TESTFILES_ROOT = Path(__file__).parent / 'test_files'
+
+
 class TestUtilClasses(unittest.TestCase):
-    """Test utility classes and their methods
+    """
+    Test utility classes and their methods
     """
 
     def setUp(self):
@@ -18,15 +21,18 @@ class TestUtilClasses(unittest.TestCase):
         """Remove setup variables."""
         pass
 
-    def test_main(self, mock_webapp):
-        """Should start the webapp."""
-        main()
-        mock_webapp.run_app.assert_called()
+    def test_schemaLoader_returns_xmlschema_object(self):
+        """Test Schemaloader return type."""
+        schema_name = "submission"
+        schemaloader = SchemaLoader()
+        schema = schemaloader.get_schema(schema_name)
+        self.assertIs(type(schema), xmlschema.XMLSchema)
 
-    async def test_init(self):
-        """Test init type."""
-        server = await init()
-        self.assertIs(type(server), web.Application)
+    def test_schemaLoader_raises_error_with_invalid_schema(self):
+        schema_name = "NULL"
+        schemaloader = SchemaLoader()
+        self.assertRaises(ValueError, schemaloader.get_schema,
+                          schema_name)
 
 
 if __name__ == '__main__':
