91bef0476e1fb5755617d6e0e620981758084d4d
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-05-04 20:43:51 +0300

    Remove non-needed settings from tox and repo
    Remove bandit and remove a alot of comments from setup.py

--- setup.py
+++ setup.py
@@ -1,51 +1,36 @@
 from setuptools import setup
 from metadata_backend import __version__, __author__, __title__
 
-# main module source folder, will be used for imports as the main module
 _main_module = 'metadata_backend'
 
 with open("requirements.txt") as reqs:
     requirements = reqs.read().splitlines()
 
 setup(
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
-    name='metadata_backend',  # Required
+    name='metadata_backend',
+    version=__version__,
+    description=__title__,
+    author=__author__,
 
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=__version__,  # Required
-
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
-    description=__title__,  # Required
-
-    author=__author__,  # Optional
-
-    # Alternative for listing individual packages
     packages=[_main_module],
 
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=requirements,
 
-    extras_require={  # Optional
+    extras_require={
         'test': ['coverage', 'pytest', 'pytest-asyncio', 'pytest-cov',
                  'coveralls', 'tox']
     },
 
-    package_data={'': ['schemas/*.xsd']},  # Optional
+    package_data={'': ['schemas/*.xsd']},
     include_package_data=True,
 
-    entry_points={  # Optional
+    entry_points={
         'console_scripts': [
-            'metadata_backend=metadata_backend.server:main',
+            'metadata_submitter=metadata_backend.server:main',
         ],
+    },
+
+    project_urls={
+        'Source': 'https://github.com/CSCfi/metadata_submitter',
     },
 )
