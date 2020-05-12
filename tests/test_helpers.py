91f090f8fcfd4adc84d0d64fd048772c26142c0a
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-05-12 16:15:21 +0300

    Break tests to many modules and fix server test await problem
    Earlier test_server reported not being awaited, since server.init() was not
    mocked.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,20 +2,13 @@ import unittest
 
 import xmlschema
 from metadata_backend.helpers.schema_load import SchemaLoader
+from metadata_backend.helpers.schema_load import SchemaNotFoundException
 
 
 class TestUtilClasses(unittest.TestCase):
     """
-    Test utility classes and their methods
+    Test helper classes and their methods.
     """
-
-    def setUp(self):
-        """Initialise fixtures."""
-        pass
-
-    def tearDown(self):
-        """Remove setup variables."""
-        pass
 
     def test_schemaLoader_returns_xmlschema_object(self):
         """Test Schemaloader return type."""
@@ -24,10 +17,11 @@ class TestUtilClasses(unittest.TestCase):
         schema = schemaloader.get_schema(schema_name)
         self.assertIs(type(schema), xmlschema.XMLSchema)
 
-    def test_schemaLoader_raises_error_with_invalid_schema(self):
+    def test_schemaLoader_raises_error_with_nonexistent_schema(self):
         schema_name = "NULL"
         schemaloader = SchemaLoader()
-        self.assertRaises(ValueError, schemaloader.get_schema, schema_name)
+        self.assertRaises(SchemaNotFoundException, schemaloader.get_schema,
+                          schema_name)
 
 
 if __name__ == '__main__':
