3a86a75f580f001e9f97a5c1fefd9f8bd3ac3fd7
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-05-18 15:25:45 +0300

    Fix mypy errors and add some try-catch checks
    This commit is a bit bigger collection of various things and
    does following:
    - fixes some mypy-errors and adds missing type hints
    - adds some try-catch checks for database and HTTP errors
    - adds accession number generation
    - makes it possible to submit multiple file same schema in one
      submission
    - adds receipt generation
    - adds some better submission.xml parsing
    
    Receipt generator and submission.xml will probably not be used during
    next iteration, and focus will be moved on to more REST-compliant
    endpoint features. Yet, they'll be present at code base in case they're
    needed at some point.

--- metadata_backend/api/translator.py
+++ metadata_backend/api/translator.py
@@ -2,7 +2,11 @@
 
 from typing import Dict
 
+from aiohttp import web
+from pymongo import errors
+
 from ..database.db_services import CRUDService, DBService
+from ..helpers.logger import LOG
 from .parser import SubmissionXMLToJSONParser
 
 
@@ -20,19 +24,32 @@ class ActionToCRUDTranslator:
         self.parser = SubmissionXMLToJSONParser()
         self.submissions = submissions
 
-    def add(self, target) -> bool:
+    def add(self, target: Dict) -> None:
         """Submit new metadata object with ADD action.
 
-        :param target: Attributes for action, e.g. information about what to
-        process in action
-        :returns: True
+        :param target: Attributes for add action, e.g. information on which
+        file to save to database
+        :raises: HTTP error when inserting file to database fails
         """
         xml_type = target["schema"]
-        content_xml = self.submissions[xml_type]
+        source = target["source"]
+        content_xml = self.submissions[xml_type][source]
         content_json = self.parser.parse(xml_type, content_xml)
         backup_json = {"accession": content_json["accession"],
                        "alias": content_json["alias"],
                        "content": content_xml}
-        CRUDService.create(self.submission_db_service, xml_type, content_json)
-        CRUDService.create(self.backup_db_service, xml_type, backup_json)
-        return True
+        try:
+            CRUDService.create(self.submission_db_service, xml_type,
+                               content_json)
+        except errors.PyMongoError as error:
+            LOG.info(f"error, reason: {error}")
+            reason = f"Error happened when saving file {source} to database."
+            raise web.HTTPBadRequest(reason=reason)
+
+        try:
+            CRUDService.create(self.backup_db_service, xml_type, backup_json)
+        except errors.PyMongoError as error:
+            LOG.info(f"error, reason: {error}")
+            reason = f"Error happened when backing up {source} to database."
+            raise web.HTTPBadRequest(reason=reason)
+        LOG.info(f"Inserting file {source} to database succeeded")
--- metadata_backend/api/views.py
+++ metadata_backend/api/views.py
@@ -1,34 +1,44 @@
 """Handle HTTP methods for server."""
-from typing import Dict, Set, List, Tuple
+from datetime import datetime
+from typing import Dict, List, cast
 
-from aiohttp import web
-from aiohttp.web import Request
+from aiohttp import BodyPartReader, web
+from aiohttp.web import Request, StreamResponse
 
 from .parser import SubmissionXMLToJSONParser
 from .translator import ActionToCRUDTranslator
-from datetime import datetime
-
-from ..helpers.logger import get_attributes, LOG
 
 
 class SiteHandler:
     """Backend HTTP method handler."""
 
     @staticmethod
-    async def extract_submissions(req: Request) -> Dict[str, List]:
+    async def extract_submissions(req: Request) -> Dict[str, Dict[str, str]]:
         """Extract submitted xml-files from multi-part request.
 
         :param request: Multi-part POST request
         :returns: Filename and content for each submitted xml, grouped by
         schemas
         """
-        submissions: Dict[str, List] = {}
+        # Schemas are used also in action sorting, so they probably should be
+        # used via class later. Refactor this in the future:
+        ok_types = {"submission", "study", "sample", "experiment", "run",
+                    "analysis", "dac", "policy", "dataset", "project"}
+        submissions: Dict[str, Dict[str, str]] = {}
         reader = await req.multipart()
         while True:
             part = await reader.next()
+            # Following is probably error in aiohttp type hints, fixing so
+            # mypy doesn't complain about it. No runtime consequences.
+            part = cast(BodyPartReader, part)
             if not part:
                 break
             xml_type = part.name.lower()
+            # Check if sent form contains correct information
+            if xml_type not in ok_types:
+                raise web.HTTPBadRequest(reason="Not ok type")
+            if part.filename is None:
+                raise web.HTTPBadRequest(reason="Filename should be incluced.")
             filename = part.filename
             data = []
             while True:
@@ -38,16 +48,17 @@ class SiteHandler:
                 data.append(chunk)
             xml_content = ''.join(x.decode('UTF-8') for x in data)
             if xml_type not in submissions:
-                submissions[xml_type] = []
-            submissions[xml_type].append((filename, xml_content))
+                submissions[xml_type] = {}
+            submissions[xml_type][filename] = xml_content
         return submissions
 
-    def generate_receipt(self, successful_submissions: Set) -> str:
+    def generate_receipt(self, successful: List, unsuccessful: List) -> str:
         """Generate receipt XML after all submissions have ran through.
 
         Not currently valid receipt (against schema), will be changed later.
 
-        :param: Set of succesful submissions
+        :param successful: Succesful submissions and their info
+        :param unsuccessful: Unuccesful submissions and their info
         :returns: XML-based receipt
         """
         date = datetime.now()
@@ -57,7 +68,7 @@ class SiteHandler:
                    f"</RECEIPT>")
         return receipt
 
-    async def submit(self, request):
+    async def submit(self, req: Request) -> StreamResponse:
         """Handle submission to server.
 
         First submission info is parsed and then for every action in submission
@@ -68,7 +79,7 @@ class SiteHandler:
         :raises: HTTP Exceptions with status code 201 or 400
         :returns: XML-based receipt from submission
         """
-        submissions = await self.extract_submissions(request)
+        submissions = await self.extract_submissions(req)
 
         if "submission" not in submissions:
             reason = "There must be a submission.xml file in submission."
@@ -78,24 +89,28 @@ class SiteHandler:
             reason = "You should submit only one submission.xml file."
             raise web.HTTPBadRequest(reason=reason)
 
-        # parser = SubmissionXMLToJSONParser()
-        # translator = ActionToCRUDTranslator(submissions)
-        # submission_json = parser.parse("submission", submissions["submission"])
+        parser = SubmissionXMLToJSONParser()
+        translator = ActionToCRUDTranslator(submissions)
+        submission_xml = list(submissions["submission"].values())[0]
+        submission_json = parser.parse("submission", submission_xml)
 
-        successful_submissions = set()
+        successful: List = []
+        unsuccessful: List = []
 
-        # for action_info in submission_json["action_infos"]:
-        #     try:
-        #         action = action_info["action"]
-        #         if getattr(translator, action)(action_info):
-        #             successful_submissions.add(action_info["schema"])
-
-        #     except AttributeError as error:
-        #         reason = (f"Unfortunately this feature has not yet been "
-        #                   f"implemented. More info: {error}")
-        #         raise web.HTTPBadRequest(reason=reason)
+        for action_info in submission_json["action_infos"]:
+            try:
+                action = action_info["action"]
+                if getattr(translator, action)(action_info):
+                    successful.append(action)
+                else:
+                    unsuccessful.append(action)
+                    break
+            except AttributeError as error:
+                reason = (f"Unfortunately this feature has not yet been "
+                          f"implemented. More info: {error}")
+                raise web.HTTPBadRequest(reason=reason)
 
         # translator.add({"schema": "submission"})
-        receipt = self.generate_receipt(successful_submissions)
+        receipt = self.generate_receipt(successful, unsuccessful)
 
-        raise web.HTTPCreated(body=receipt, content_type="text/xml")
+        return web.Response(body=receipt, status=201, content_type="text/xml")
