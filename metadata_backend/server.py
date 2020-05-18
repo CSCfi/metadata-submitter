7f2f45177d1877959d0819a2d140d0dd219dd5c7
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-05-18 15:25:45 +0300

    Fix mypy errors and add some try-catch checks
    This commit is a bit bigger collection of various things and
    does following:
    - fixes some mypy-errors and adds missing type hints
    - adds some try-catch checks for database and HTTP errors
    - adds accession number generation
    - makes it possible to submit multiple file same schema in one
      submission
    - adds receipt generation
    - adds some better submission.xml parsing
    
    Receipt generator and submission.xml will probably not be used during
    next iteration, and focus will be moved on to more REST-compliant
    endpoint features. Yet, they'll be present at code base in case they're
    needed at some point.

--- metadata_backend/server.py
+++ metadata_backend/server.py
@@ -3,30 +3,29 @@
 import asyncio
 
 import uvloop
-from aiohttp.web import Application, RouteTableDef, run_app
+from aiohttp import web
 
 from .api.views import SiteHandler
 from .helpers.logger import LOG
 
-routes = RouteTableDef()
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
-async def init() -> Application:
+async def init() -> web.Application:
     """Initialise server and setup routes."""
-    server = Application()
+    server = web.Application()
     handler = SiteHandler()
     server.router.add_post('/submit', handler.submit)
     LOG.info("Server configurations and routes loaded")
     return server
 
 
-def main():
+def main() -> None:
     """Launch the server."""
     host = '0.0.0.0'  # nosec
     port = 5430
+    web.run_app(init(), host=host, port=port, shutdown_timeout=0)
     LOG.info(f"Started server on {host}:{port}")
-    run_app(init(), host=host, port=port, shutdown_timeout=0)
 
 
 if __name__ == '__main__':
