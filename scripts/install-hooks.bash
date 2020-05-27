d7418edb271c0b1e84fa1f4f485b58c7463613c5
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-05-27 17:42:00 +0300

    Write some contributing tips and add misspell
    Misspell checks for spelling errors and can be ran with
    pre-commit hook. This also adds pre-commit hook install tip for
    possible contributors to contributing guide.

--- scripts/install-hooks.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-git_root=$(git rev-parse --show-toplevel)
-
-echo "Installing hooks..."
-# this command creates symlink to our pre-commit script
-ln -s $git_root/scripts/pre-commit.bash $git_root/.git/hooks/pre-commit
-echo "Done!
