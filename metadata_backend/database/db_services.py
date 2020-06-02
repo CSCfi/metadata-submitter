e46edecb917d6b71352697f83ef55e60f95ae15d
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-02 08:59:47 +0300

    Add more crud operations and tests for db_services

--- metadata_backend/database/db_services.py
+++ metadata_backend/database/db_services.py
@@ -62,9 +62,9 @@ class CRUDService:
     @staticmethod
     def read(db_service: DBService, collection: str,
              query: Dict) -> Cursor:
-        """Insert document to collection in database.
+        """Query from collection in database.
 
-        :param db_service: Database object which connect to MongoDB database
+        :param db_service: Service that connects to MongoDB database
         :param collection: Collection where document should be searched from
         :param query: Query for document(s) that should be found
         :returns: Pymongo's Cursor object (iterator)
@@ -72,5 +72,60 @@ class CRUDService:
         """
         try:
             return db_service.database[collection].find(query)
+        except errors.ConnectionFailure:
+            raise
+
+    @staticmethod
+    def update(db_service: DBService, collection: str,
+               accession_id: str, data_to_be_updated: Dict) -> None:
+        """Update some elements of object by its accessionId.
+
+        :param db_service: Service that connects to MongoDB database
+        :param collection: Collection where document should be searched from
+        :param accession_id: Accession id for object to be updated
+        :param data_to_be_updated: JSON representing the data that should be
+        updated to object, can replace previous fields and add new ones.
+        :raises: Error when read fails for any Mongodb related reason
+        """
+        try:
+            find_by_id_query = {"accessionId": accession_id}
+            update_operation = {"$set": data_to_be_updated}
+            db_service.database[collection].update_one(find_by_id_query,
+                                                       update_operation)
+        except errors.ConnectionFailure:
+            raise
+
+    @staticmethod
+    def replace(db_service: DBService, collection: str,
+                accession_id: str, data_to_be_updated: Dict) -> None:
+        """Replace whole object by its accessionId.
+
+        :param db_service: Service that connects to MongoDB database
+        :param collection: Collection where document should be searched from
+        :param accession_id: Accession id for object to be updated
+        :param data_to_be_updated: JSON representing the data that replaces
+        old data
+        :raises: Error when read fails for any Mongodb related reason
+        """
+        try:
+            find_by_id_query = {"accessionId": accession_id}
+            db_service.database[collection].replace_one(find_by_id_query,
+                                                        data_to_be_updated)
+        except errors.ConnectionFailure:
+            raise
+
+    @staticmethod
+    def delete(db_service: DBService, collection: str,
+               accession_id: str) -> None:
+        """Delete object by its accessionId.
+
+        :param db_service: Service that connects to MongoDB database
+        :param collection: Collection where document should be searched from
+        :param accession_id: Accession id for object to be updated
+        :raises: Error when read fails for any Mongodb related reason
+        """
+        try:
+            find_by_id_query = {"accessionId": accession_id}
+            db_service.database[collection].delete_one(find_by_id_query)
         except errors.ConnectionFailure:
             raise
