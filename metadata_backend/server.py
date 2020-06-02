f32310c59ee56593a46a4ca233720aa5282cbaba
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-02 14:26:55 +0300

    Change /{schema} -url handling logic
    /{schema} -urls are handled in server.py and they should work the
    same way /object/{schema} are working. Difference is that
    previously /{schema} worked as a redirecter.

--- metadata_backend/server.py
+++ metadata_backend/server.py
@@ -17,18 +17,19 @@ async def init() -> web.Application:
     Routes should be setup by adding similar paths one after the another. (i.e.
     POST and GET for same path grouped together). Handler method names should
     be used for route names, so they're easy to use in other parts of the
-    application.
-
+    application. Note: if using variable resources (such as {schema}), add
+    specific ones on top of more generic ones.
     """
     server = web.Application()
     handler = SiteHandler()
     routes = [web.get('/objects', handler.get_object_types),
               web.get('/object/{schema}/{accessionId}', handler.get_object,
                       name='get_object'),
-              web.post('/object/{schema}', handler.submit_object),
-              web.get('/{schema}/{accessionId}',
-                      handler.get_object_from_alias_address),
-              web.post('/submit', handler.submit)]
+              web.post('/object/{schema}', handler.submit_object,
+                       name='submit_object'),
+              web.post('/submit', handler.submit),
+              web.get('/{schema}', handler.get_object),
+              web.post('/{schema}', handler.submit_object)]
     server.router.add_routes(routes)
     LOG.info("Server configurations and routes loaded")
     return server
