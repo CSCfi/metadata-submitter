757e2c964f333df5d6600687dfb3f7a44fb68adf
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-02 10:36:24 +0300

    Move db_client configurations to config module
    MongoClient should be imported from config module whenever it is
    used.

--- metadata_backend/database/db_services.py
+++ metadata_backend/database/db_services.py
@@ -1,25 +1,11 @@
-"""Services that handle database connections. Implemented with MongoDB.
+"""Services that handle database connections. Implemented with MongoDB."""
 
-MongoDB client should be shared across the whole application, so it's created
-here as module level variable.
-
-Admin access is needed in order to create new databases during runtime.
-Default values are the same that are used in docker-compose file
-found from deploy/mongodb.
-"""
-
-import os
 from typing import Dict
 
-from pymongo import MongoClient, errors
+from pymongo import errors
 from pymongo.cursor import Cursor
 
-# Set up database client
-mongo_user = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin")
-mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin")
-mongo_host = os.getenv("MONGODB_HOST", "localhost:27017")
-url = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}"
-db_client = MongoClient(url)
+from ..conf.conf import db_client
 
 
 class DBService:
