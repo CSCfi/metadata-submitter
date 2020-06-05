ed3a6fddc07171e26a05e965f805345f3496445f
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-05 10:05:39 +0300

    Add rst syntax to server dosstrings.

--- metadata_backend/server.py
+++ metadata_backend/server.py
@@ -17,7 +17,8 @@ async def init() -> web.Application:
     Routes should be setup by adding similar paths one after the another. (i.e.
     POST and GET for same path grouped together). Handler method names should
     be used for route names, so they're easy to use in other parts of the
-    application. Note: if using variable resources (such as {schema}), add
+    application.
+    Note:: if using variable resources (such as {schema}), add
     specific ones on top of more generic ones.
     """
     server = web.Application()
