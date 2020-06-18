357d35f97073a6b17dad77c5b0ef6c31328c78ff
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-18 10:54:36 +0300

    Add frontend build to dockerfile
    Also restored gunicorn.

--- deploy/app.sh
+++ deploy/app.sh
@@ -2,6 +2,5 @@
 THE_HOST=${HOST:="0.0.0.0"}
 THE_PORT=${PORT:="5430"}
 
-# TODO: fix that this does not use gunicorn
 echo 'Start metadata backend'
 exec gunicorn metadata_backend.server:init --bind $THE_HOST:$THE_PORT --worker-class aiohttp.GunicornUVLoopWebWorker --workers 4
