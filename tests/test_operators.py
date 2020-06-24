17aa81cf6d7b39e92043bebf355e5f1a087aa8d0
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-24 09:50:59 +0300

    Remove mongo internal ids from returned objects

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -6,7 +6,7 @@ from metadata_backend.api.operators import Operator
 
 
 class TestOperators(unittest.TestCase):
-    """Test schema loader."""
+    """Test db-operator classes."""
 
     def setUp(self):
         """Configure default values for testing and mock dbservice."""
@@ -14,8 +14,8 @@ class TestOperators(unittest.TestCase):
         self.patch_dbservice = patch(class_dbservice, spec=True)
         self.MockedDbService = self.patch_dbservice.start()
 
-    def test_operator_fixes_datetime_presentation(self):
-        """Test datetime is fixed from datetime object to ISO8601."""
+    def test_operator_fixes_single_document_presentation(self):
+        """Test datetime is fixed and id removed."""
         study_test = {
             "_id": {
                 "$oid": "5ecd28877f55c72e263f45c2"
@@ -24,8 +24,10 @@ class TestOperators(unittest.TestCase):
             "accessionId": "EDAG3945644754983408"
         }
         operator = Operator()
-        result = operator._format_publish_date(study_test)
+        result = operator._format_single_dict("study", study_test)
         assert result["publishDate"] == "2020-06-14T00:00:00"
+        with self.assertRaises(KeyError):
+            result["_Id"]
 
 
 if __name__ == '__main__':
