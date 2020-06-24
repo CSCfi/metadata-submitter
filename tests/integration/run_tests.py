8d0834a38384994628f0c35a73d76fdfeac91d8c
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-24 07:49:12 +0300

    Add publish date parsing to queries and integration tests for queries

--- tests/integration/run_tests.py
+++ tests/integration/run_tests.py
@@ -42,20 +42,38 @@ async def test_post_and_get_works(schema, filename):
     :param filename: name of the file used for testing.
     """
     async with aiohttp.ClientSession() as session:
-        LOG.debug(f"Testing POST with {schema}")
         base_url = "http://localhost:5430/object"
         data = await create_submission_data(schema, filename)
         async with session.post(f"{base_url}/{schema}", data=data) as resp:
+            LOG.debug(f"Testing POST with {schema}")
             assert resp.status == 201, 'HTTP Status code error'
             ans = await resp.json()
             accession_id = ans["accessionId"]
-        LOG.debug(f"Testing GET with {schema}")
         async with session.get(f"{base_url}/{schema}/{accession_id}") as resp:
+            LOG.debug(f"Testing GET with {schema}")
             assert resp.status == 200, 'HTTP Status code error'
 
 
-async def main():
-    """Launch 500 test tasks and run them asyncronously."""
+async def test_querying_works():
+    """Test query endpoint with working and failing query."""
+    async with aiohttp.ClientSession() as session:
+        base_url = "http://localhost:5430/object"
+        LOG.debug("Querying studies")
+        query = "study?studyTitle=yoloswaggingsandthefellowshipofthebling"
+        async with session.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 404, 'HTTP Status code error'
+        query = "study?studyTitle=integrated"
+        async with session.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+
+async def main(test_repeats: int):
+    """Launch different test tasks and run them.
+
+    :param test_repeats: how many times to repeat tests. Set high (e.g. 500)
+    if you want to stress test server.
+    """
+    # Test adding and getting files
     test_files = [
         ("study", "SRP000539.xml"),
         ("sample", "SRS001433.xml"),
@@ -65,8 +83,11 @@ async def main():
     ]
     await asyncio.gather(
         *[test_post_and_get_works(schema, file) for schema, file in test_files
-          for _ in range(100)]
+          for _ in range(test_repeats)]
     )
 
+    # Test queries
+    await asyncio.gather(*[test_querying_works() for _ in range(test_repeats)])
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(1))
