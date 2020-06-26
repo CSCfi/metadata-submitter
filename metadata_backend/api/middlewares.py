5bafe217d0242835701f322b8c15473bbd165ebf
Joonatan Makinen <joonatan.makinen@csc.fi>
2020-06-26 13:52:49 +0300

    change problem details to appear in 'errors' array as described in openapi specs and fix unit tests

--- metadata_backend/api/middlewares.py
+++ metadata_backend/api/middlewares.py
@@ -23,18 +23,31 @@ def error_middleware() -> Callable:
         :param handler: A request handler
         :returns: JSON response for the error
         """
+        errors = []
         try:
             response = await handler(req)
-            return response
         except HTTPError as error:
-            return _json_exception(error.status, error, req.url)
+            errors.append(_json_exception(error.status, error, req.url))
+        finally:
+            if errors:
+                # get error status code and remove it from the dict
+                for e in errors:
+                    status = e.pop('status', None)
+                # insert array under errors key
+                body = json.dumps({
+                    'errors': errors
+                }).encode('utf-8')
+                return Response(status=status,
+                                body=body,
+                                content_type='application/problem+json')
+            else:
+                return response
 
     return http_error_handler
 
 
-def _json_exception(status: int, exception: HTTPException,
-                    url: URL) -> Response:
-    """Convert an HTTP exception into a problem detailed JSON response.
+def _json_exception(status: int, exception: HTTPException, url: URL) -> dict:
+    """Convert an HTTP exception into a problem detailed JSON object.
 
     The problem details are in accordance with RFC 7807.
     (https://tools.ietf.org/html/rfc7807)
@@ -42,16 +55,14 @@ def _json_exception(status: int, exception: HTTPException,
     :param status: Status code of the HTTP exception
     :param exception: Exception content
     :param url: Request URL that caused the exception
-    :returns: Response in problem detail JSON format
+    :returns: Dictionary of problem details
     """
-    body = json.dumps({
-        'type': "about:blank",
-        # Replace type value above with an URL to
-        # a custom error document when one exists
-        'title': HTTPStatus(status).phrase,
-        'detail': exception.reason,
-        'instance': url.path,  # optional
-    }).encode('utf-8')
+    body = {'type': "about:blank",
+            # Replace type value above with an URL to
+            # a custom error document when one exists
+            'title': HTTPStatus(status).phrase,
+            'detail': exception.reason,
+            'instance': url.path,  # instance is optional
+            'status': status}
     LOG.info(str(body))
-    return Response(status=status, body=body,
-                    content_type='application/problem+json')
+    return body
