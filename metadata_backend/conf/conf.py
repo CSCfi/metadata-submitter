7589c7451598c4101424bb35c2cf98fc7e3b027d
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-07-01 13:46:57 +0300

    Change db_client init, save db_client to aiohttp App

--- metadata_backend/conf/conf.py
+++ metadata_backend/conf/conf.py
@@ -8,11 +8,14 @@ Currently in use:
 - MONGO_INITDB_ROOT_PASSWORD - Admin password for mongodb
 - MONGODB_HOST - Mongodb server hostname, with port spesified if needed
 
-MongoDB client should be shared across the whole application and always
-imported from this module.
 Admin access is needed in order to create new databases during runtime.
 Default values are the same that are used in docker-compose file
 found from deploy/mongodb.
+
+MongoDB client should be shared across the whole application. Since aiohttp
+discourages usage of singletons, recommended way is to initialize database
+when setting up server and store db to application instance in server.py
+module.
 
 2) Metadata schema types
 Schema types (such as "submission", "study", "sample") are needed in
@@ -33,17 +36,21 @@ from pathlib import Path
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
+
 # 1) Set up database client
-mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME", "admin")
-mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin")
-mongo_host = os.getenv("MONGODB_HOST", "localhost:27017")
-url = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}"
-db_client = AsyncIOMotorClient(url)
+def create_db_client():
+    mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME", "admin")
+    mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin")
+    mongo_host = os.getenv("MONGODB_HOST", "localhost:27017")
+    url = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}"
+    return AsyncIOMotorClient(url)
+
 
 # 2) Load schema types and descriptions from json
 path_to_schema_file = Path(__file__).parent / "schemas.json"
 with open(path_to_schema_file) as schema_file:
     schema_types = json.load(schema_file)
+
 
 # 3) Define mapping between url query parameters and mongodb queries
 query_map = {
@@ -75,6 +82,7 @@ query_map = {
                           "keys": ["accessionId", "refname",
                                    "refcenter"]},
 }
+
 
 # 4) Set frontend folder to be inside metadata_backend modules root
 frontend_static_files = Path(__file__).parent.parent / "frontend"
