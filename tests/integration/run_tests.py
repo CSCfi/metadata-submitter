008491cc2fc49b4c1807d707c1f00b1edcf56427
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-07-08 09:46:21 +0300

    Add integration tests for different query parameters

--- tests/integration/run_tests.py
+++ tests/integration/run_tests.py
@@ -20,6 +20,14 @@ LOG.setLevel(logging.DEBUG)
 
 TESTFILES_ROOT = Path(__file__).parent.parent / 'test_files'
 
+test_files = [
+    ("study", "SRP000539.xml"),
+    ("sample", "SRS001433.xml"),
+    ("run", "ERR000076.xml"),
+    ("experiment", "ERX000119.xml"),
+    ("analysis", "ERZ266973.xml")
+]
+
 
 async def create_submission_data(schema, filename):
     """Create request data from pairs of schemas and filenames.
@@ -74,24 +82,110 @@ async def test_querying_works():
     """Test query endpoint with working and failing query."""
     async with aiohttp.ClientSession() as sess:
         base_url = "http://localhost:5430/objects"
+        accession_ids = []
 
-        # Study-related query endpoints
-        data = await create_submission_data("study", "SRP000539.xml")
-        async with sess.post(f"{base_url}/study", data=data) as resp:
-            LOG.debug("Adding new object to study")
-            assert resp.status == 201, 'HTTP Status code error'
-            ans = await resp.json()
-            accession_id = ans["accessionId"]
-        LOG.debug("Querying study collection")
+        async def post_one(schema, filename):
+            data = await create_submission_data(schema, filename)
+            async with sess.post(f"{base_url}/{schema}", data=data) as resp:
+                assert resp.status == 201, 'HTTP Status code error'
+                ans = await resp.json()
+                accession_ids.append((schema, ans["accessionId"]))
+
+        await asyncio.gather(*[post_one(schema, filename) for
+                               schema, filename in test_files])
+
+        # Generic query endpoints
+        LOG.debug("Querying collections with generic params")
+        query = "sample?title=HapMap sample"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "sample?description=human hapmap individual"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "sample?centerName=hapmap"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # TODO: query name param here
+
+        # Study related query endpoints
+
+        LOG.debug("Querying study collection with non-working params")
         query = "study?studyTitle=yoloswaggingsandthefellowshipofthebling"
         async with sess.get(f"{base_url}/{query}") as resp:
             assert resp.status == 404, 'HTTP Status code error'
+        query = "study?studyType=yoloswaggingsandthefellowshipofthebling"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 404, 'HTTP Status code error'
+        query = "study?studyAbstract=yoloswaggingsandthefellowshipofthebling"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 404, 'HTTP Status code error'
+        query = "study?studyAttributes=yoloswaggingsandthefellowshipofthebling"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 404, 'HTTP Status code error'
+
+        LOG.debug("Querying study collection with working params")
         query = "study?studyTitle=integrated"
         async with sess.get(f"{base_url}/{query}") as resp:
             assert resp.status == 200, 'HTTP Status code error'
-        async with sess.delete(f"{base_url}/study/{accession_id}") as resp:
-            LOG.debug(f"Deleting object {accession_id} in study")
-            assert resp.status == 204, 'HTTP Status code error'
+        query = "study?studyType=Other"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "study?studyAbstract=arabidopsis thaliana"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "study?studyAttributes=prjna107265"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "study?studyAttributes=parent_bioproject"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # Sample-related query endpoints
+        LOG.debug("Querying sample collection with working params")
+        query = "sample?sampleName=homo sapiens"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "sample?scientificName=homo sapiens"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # Run-related query endpoints
+        LOG.debug("Querying run collection with working params")
+        query = "run?fileType=srf"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "run?experimentReference=g1k-bgi-na18542"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "run?experimentReference=erx000037"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # Experiment-related query endpoints
+        LOG.debug("Querying experiment collection with working params")
+        query = "experiment?studyReference=1000Genomes project pilot"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # Analysis-related query endpoints
+        LOG.debug("Querying analysis collection with working params")
+        query = "analysis?studyReference=HipSci___RNAseq___"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+        query = "analysis?sampleReference=HPSI0114i-eipl_3"
+        async with sess.get(f"{base_url}/{query}") as resp:
+            assert resp.status == 200, 'HTTP Status code error'
+
+        # Delete files
+        async def delete_one(schema, accession_id):
+            url = f"{base_url}/{schema}/{accession_id}"
+            async with sess.delete(f"{url}") as resp:
+                assert resp.status == 204, 'HTTP Status code error'
+
+        LOG.debug("Deleting objects from study")
+        await asyncio.gather(*[delete_one(schema, accession_id) for
+                               schema, accession_id in accession_ids])
 
 
 async def test_getting_all_objects_from_schema_works():
@@ -146,19 +240,8 @@ async def test_getting_all_objects_from_schema_works():
 
 
 async def main():
-    """Launch different test tasks and run them.
-
-    Change value in range for how many times to repeat the tests.
-    Set to high value (e.g. 500) if you want to stress test server.
-    """
+    """Launch different test tasks and run them."""
     # Test adding and getting files
-    test_files = [
-        ("study", "SRP000539.xml"),
-        ("sample", "SRS001433.xml"),
-        ("run", "ERR000076.xml"),
-        ("experiment", "ERX000119.xml"),
-        ("analysis", "ERZ266973.xml")
-    ]
     await asyncio.gather(
         *[test_post_and_get_works(schema, file) for schema, file in test_files]
     )
@@ -166,7 +249,7 @@ async def main():
     # Test queries
     await test_querying_works()
 
-    # Test /objects/study endpoint
+    # Test /objects/study endpoint for query pagination
     await test_getting_all_objects_from_schema_works()
 
 if __name__ == '__main__':
