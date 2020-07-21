8b7ad161185505d7d4ece2a8654da96cc6b45adc
Stefan Negru <stefan@blankdots.com>
2020-07-21 15:51:02 +0300

    JSON schema loader

--- metadata_backend/helpers/schema_loader.py
+++ metadata_backend/helpers/schema_loader.py
@@ -7,6 +7,7 @@ probably be replaced with database searching in the future.
 from pathlib import Path
 
 from xmlschema import XMLSchema
+import json
 
 SCHEMAS_ROOT = Path(__file__).parent / 'schemas'
 
@@ -16,11 +17,11 @@ class SchemaNotFoundException(Exception):
 
     def __init__(self) -> None:
         """Set up exception message."""
-        Exception.__init__(self, "There is no xsd file for given schema.")
+        Exception.__init__(self, "There is no file for given schema.")
 
 
 class XMLSchemaLoader:
-    """Loader implementation."""
+    """XML Schema Loader implementation."""
 
     def __init__(self) -> None:
         """Load schemas folder on initialization."""
@@ -48,3 +49,28 @@ class XMLSchemaLoader:
         if not schema_file:
             raise SchemaNotFoundException
         return XMLSchema(schema_file, base_url=self.path.as_posix())
+
+
+class JSONSchemaLoader:
+    """JSON Loader implementation."""
+
+    def __init__(self) -> None:
+        """Load schemas folder on initialization."""
+        self.path = SCHEMAS_ROOT
+
+    def get_schema(self, schema_type: str) -> dict:
+        """Find schema which is used to match JSON files against.
+
+        :param schema_type: Schema type to be searched for
+        :returns: JSONSchema able to validate XML against defined schema type
+        :raises SchemaNotFoundException: If searched schema doesn't exist
+        """
+        schema_type = schema_type.lower()
+        schema_file = None
+        for file in [x for x in self.path.iterdir()]:
+            if schema_type in file.name and file.name.endswith("json"):
+                with file.open() as f:
+                    schema_file = json.load(f)
+        if not schema_file:
+            raise SchemaNotFoundException
+        return schema_file
