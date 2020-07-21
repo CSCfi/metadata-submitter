03485aeff1aa6beb4863b5ecb344fae08d0de651
Joonatan Makinen <joonatan.makinen@csc.fi>
2020-07-21 17:57:16 +0300

    move middleware methods and remap them to server instance more clearly

--- metadata_backend/api/middlewares.py
+++ metadata_backend/api/middlewares.py
@@ -13,47 +13,41 @@ from yarl import URL
 from ..helpers.logger import LOG
 
 
-def error_middleware() -> Callable:
+@middleware
+async def http_error_handler(req: Request, handler: Callable) -> Response:
     """Middleware for handling exceptions recieved from the API methods.
 
-    :returns: Handled response
+    :param req: A request instance
+    :param handler: A request handler
+    :raises: Reformatted HTTP Exceptions
+    :returns: Successful requests unaffected
     """
-
-    @middleware
-    async def http_error_handler(req: Request, handler: Callable) -> Response:
-        """Handle HTTP errors.
-
-        :param req: A request instance
-        :param handler: A request handler
-        :raises: Reformatted HTTP Exceptions
-        :returns: Successful requests unaffected
-        """
-        try:
-            response = await handler(req)
-            return response
-        except web.HTTPError as error:
-            details = _json_exception(error.status, error, req.url)
-            LOG.error(details)
-            c_type = 'application/problem+json'
-            if error.status == 400:
-                raise web.HTTPBadRequest(text=details,
-                                         content_type=c_type)
-            elif error.status == 401:
-                raise web.HTTPUnauthorized(text=details,
-                                           content_type=c_type)
-            elif error.status == 403:
-                raise web.HTTPForbidden(text=details,
-                                        content_type=c_type)
-            elif error.status == 404:
-                raise web.HTTPNotFound(text=details,
+    try:
+        response = await handler(req)
+        return response
+    except web.HTTPError as error:
+        details = _json_exception(error.status, error, req.url)
+        LOG.error(details)
+        c_type = 'application/problem+json'
+        if error.status == 400:
+            raise web.HTTPBadRequest(text=details,
+                                     content_type=c_type)
+        elif error.status == 401:
+            raise web.HTTPUnauthorized(text=details,
                                        content_type=c_type)
-            else:
-                raise web.HTTPServerError()
+        elif error.status == 403:
+            raise web.HTTPForbidden(text=details,
+                                    content_type=c_type)
+        elif error.status == 404:
+            raise web.HTTPNotFound(text=details,
+                                   content_type=c_type)
+        else:
+            raise web.HTTPServerError()
 
     return http_error_handler
 
-
-def jwt_middleware() -> Callable:
+@middleware
+async def jwt_authentication(req: Request, handler: Callable) -> Response:
     """Middleware for validating and authenticating JSON web token.
 
     :param req: A request instance
@@ -61,68 +55,65 @@ def jwt_middleware() -> Callable:
     :raises: HTTP Exception with status code 401 or 403
     :returns: Successful requests unaffected
     """
-    @middleware
-    async def jwt_authentication(req: Request, handler: Callable) -> Response:
-        if 'Authorization' in req.headers:
-            # Check token exists
-            try:
-                scheme, token = req.headers.get('Authorization').split(' ')
-                LOG.info('Auth token received.')
-            except Exception as err:
-                raise web.HTTPUnauthorized(reason=str(err))
+    if 'Authorization' in req.headers:
+        # Check token exists
+        try:
+            scheme, token = req.headers.get('Authorization').split(' ')
+            LOG.info('Auth token received.')
+        except Exception as err:
+            raise web.HTTPUnauthorized(reason=str(err))
 
-            # Check token has proper scheme and was provided.
-            if not re.match('Bearer', scheme):
-                raise web.HTTPUnauthorized(reason="Invalid token scheme, "
-                                                  "Bearer required.")
-            if token is None:
-                raise web.HTTPUnauthorized(reason='Token cannot be empty.')
+        # Check token has proper scheme and was provided.
+        if not re.match('Bearer', scheme):
+            raise web.HTTPUnauthorized(reason="Invalid token scheme, "
+                                              "Bearer required.")
+        if token is None:
+            raise web.HTTPUnauthorized(reason='Token cannot be empty.')
 
-            # JWK and JWTClaims parameters for decoding
-            key = environ.get('PUBLIC_KEY', None)
-            # TODO: get Oauth2 public key
-            # TODO: verify audience claim
+        # JWK and JWTClaims parameters for decoding
+        key = environ.get('PUBLIC_KEY', None)
+        # TODO: get Oauth2 public key
+        # TODO: verify audience claim
 
-            # Include claims that are required to be present
-            # in the payload of the token
-            claims_options = {
-                "iss": {
-                    "essential": True,
-                    # TODO: add the proper issuer URLs here
-                    "values": ["haka_iss", "elixir_iss"]
-                },
-                "exp": {
-                    "essential": True
-                }
+        # Include claims that are required to be present
+        # in the payload of the token
+        claims_options = {
+            "iss": {
+                "essential": True,
+                # TODO: add the proper issuer URLs here
+                "values": ["haka_iss", "elixir_iss"]
+            },
+            "exp": {
+                "essential": True
             }
+        }
 
-            # Decode and validate token
-            try:
-                claims = jwt.decode(token, key, claims_options=claims_options)
-                claims.validate()
-                LOG.info('Auth token decoded and validated.')
+        # Decode and validate token
+        try:
+            claims = jwt.decode(token, key, claims_options=claims_options)
+            claims.validate()
+            LOG.info('Auth token decoded and validated.')
 
-                # TODO: Retrieve GA4GH passports and process into permissions
+            # TODO: Retrieve GA4GH passports and process into permissions
 
-                req["token"] = {"authenticated": True}
-                return await handler(req)
-            except errors.MissingClaimError as err:
-                raise web.HTTPUnauthorized(reason=f"{err}")
-            except errors.ExpiredTokenError as err:
-                raise web.HTTPUnauthorized(reason=f"{err}")
-            except errors.InvalidClaimError as err:
-                raise web.HTTPForbidden(reason=f"Token contains {err}")
-            except errors.BadSignatureError as err:
-                raise web.HTTPUnauthorized(reason="Token signature is invalid"
-                                                  f", {err}")
-            except errors.InvalidTokenError as err:
-                raise web.HTTPUnauthorized(reason="Invalid authorization token"
-                                                  f": {err}")
-        else:
-            req["token"] = {"authenticated": False}
+            req["token"] = {"authenticated": True}
             return await handler(req)
+        except errors.MissingClaimError as err:
+            raise web.HTTPUnauthorized(reason=f"{err}")
+        except errors.ExpiredTokenError as err:
+            raise web.HTTPUnauthorized(reason=f"{err}")
+        except errors.InvalidClaimError as err:
+            raise web.HTTPForbidden(reason=f"Token contains {err}")
+        except errors.BadSignatureError as err:
+            raise web.HTTPUnauthorized(reason="Token signature is invalid"
+                                              f", {err}")
+        except errors.InvalidTokenError as err:
+            raise web.HTTPUnauthorized(reason="Invalid authorization token"
+                                              f": {err}")
+    else:
+        req["token"] = {"authenticated": False}
+        return await handler(req)
 
-    return jwt_authentication
 
 
 def _json_exception(status: int, exception: web.HTTPException,
