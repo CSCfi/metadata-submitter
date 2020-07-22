e808317944753794e9efafda20cf6dff629a8947
Joonatan Makinen <joonatan.makinen@csc.fi>
2020-07-22 10:23:31 +0300

    separate unit test classes

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -9,8 +9,38 @@ from authlib.jose import jwt
 from metadata_backend.server import init
 
 
-class MiddlewaresTestCase(AioHTTPTestCase):
-    """Api middlewares test cases."""
+class ErrorMiddlewareTestCase(AioHTTPTestCase):
+    """Error handling middleware test cases."""
+
+    async def get_application(self):
+        """Retrieve web Application for test."""
+        return await init()
+
+    @unittest_run_loop
+    async def test_bad_HTTP_request_converts_into_json_response(self):
+        """Test that middleware reformats 400 error with problem details."""
+        data = _create_improper_data()
+        response = await self.client.post("/submit", data=data)
+        self.assertEqual(response.status, 400)
+        self.assertEqual(response.content_type, "application/problem+json")
+        resp_dict = await response.json()
+        self.assertIn("Bad Request", resp_dict['title'])
+        self.assertIn("There must be a submission.xml file in submission.",
+                      resp_dict['detail'])
+        self.assertIn("/submit", resp_dict['instance'])
+
+    @unittest_run_loop
+    async def test_bad_url_returns_json_response(self):
+        """Test that unrouted api url returns a 404 in JSON format."""
+        response = await self.client.get("/objects/swagadagamaster")
+        self.assertEqual(response.status, 404)
+        self.assertEqual(response.content_type, "application/problem+json")
+        resp_dict = await response.json()
+        self.assertIn("Not Found", resp_dict['title'])
+
+
+class AuthMiddlewareTestCase(AioHTTPTestCase):
+    """Authentication middleware test cases."""
 
     async def get_application(self):
         """Retrieve web Application for test."""
@@ -39,45 +69,12 @@ class MiddlewaresTestCase(AioHTTPTestCase):
         """Unset the public key."""
         os.unsetenv('PUBLIC_KEY')
 
-    def create_improper_data(self):
-        """Create request data that produces a 404 error.
-
-        Submission method in API handlers raises Bad Request (400) error
-        if 'submission' is not included on the first field of request
-        """
-        data = FormData()
-        data.add_field("study", "content of a file",
-                       filename='file', content_type='text/xml')
-        return data
-
-    @unittest_run_loop
-    async def test_bad_HTTP_request_converts_into_json_response(self):
-        """Test that middleware reformats 400 error with problem details."""
-        data = self.create_improper_data()
-        response = await self.client.post("/submit", data=data)
-        self.assertEqual(response.status, 400)
-        self.assertEqual(response.content_type, "application/problem+json")
-        resp_dict = await response.json()
-        self.assertIn("Bad Request", resp_dict['title'])
-        self.assertIn("There must be a submission.xml file in submission.",
-                      resp_dict['detail'])
-        self.assertIn("/submit", resp_dict['instance'])
-
-    @unittest_run_loop
-    async def test_bad_url_returns_json_response(self):
-        """Test that unrouted api url returns a 404 in JSON format."""
-        response = await self.client.get("/objects/swagadagamaster")
-        self.assertEqual(response.status, 404)
-        self.assertEqual(response.content_type, "application/problem+json")
-        resp_dict = await response.json()
-        self.assertIn("Not Found", resp_dict['title'])
-
     @unittest_run_loop
     async def test_authentication_passes(self):
         """Test that JWT authenticates."""
         # Add claims to mocked token
         token = jwt.encode(self.header, self.payload, self.pem).decode('utf-8')
-        data = self.create_improper_data()
+        data = _create_improper_data()
         response = await self.client.post("/submit", data=data,
                                           headers={'Authorization':
                                                    f"Bearer {token}"})
@@ -96,7 +93,7 @@ class MiddlewaresTestCase(AioHTTPTestCase):
         payload = self.payload
         payload['exp'] = 0
         token = jwt.encode(self.header, payload, self.pem).decode('utf-8')
-        data = self.create_improper_data()
+        data = _create_improper_data()
         response = await self.client.post("/submit", data=data,
                                           headers={'Authorization':
                                                    f"Bearer {token}"})
@@ -114,7 +111,7 @@ class MiddlewaresTestCase(AioHTTPTestCase):
         payload = self.payload
         del payload['iss']
         token = jwt.encode(self.header, self.payload, self.pem).decode('utf-8')
-        data = self.create_improper_data()
+        data = _create_improper_data()
         response = await self.client.post("/submit", data=data,
                                           headers={'Authorization':
                                                    f"Bearer {token}"})
@@ -133,7 +130,7 @@ class MiddlewaresTestCase(AioHTTPTestCase):
         payload = self.payload
         payload['iss'] = "wrong_iss"
         token = jwt.encode(self.header, payload, self.pem).decode('utf-8')
-        data = self.create_improper_data()
+        data = _create_improper_data()
         response = await self.client.post("/submit", data=data,
                                           headers={'Authorization':
                                                    f"Bearer {token}"})
@@ -154,7 +151,7 @@ class MiddlewaresTestCase(AioHTTPTestCase):
             'k': "hJtXIZ2uSN5kbQfbtTNWbpdmhkV8FJG"
         }
         token = jwt.encode(self.header, self.payload, otherkey).decode('utf-8')
-        data = self.create_improper_data()
+        data = _create_improper_data()
         response = await self.client.post("/submit", data=data,
                                           headers={'Authorization':
                                                    f"Bearer {token}"})
@@ -165,3 +162,15 @@ class MiddlewaresTestCase(AioHTTPTestCase):
         resp_dict = await response.json()
         self.assertIn('Token signature is invalid, bad_signature:',
                       resp_dict['detail'])
+
+
+def _create_improper_data():
+    """Create request data that produces a 404 error.
+
+    Submission method in API handlers raises Bad Request (400) error
+    if 'submission' is not included on the first field of request
+    """
+    data = FormData()
+    data.add_field("study", "content of a file",
+                   filename='file', content_type='text/xml')
+    return data
