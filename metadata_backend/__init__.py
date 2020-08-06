0538981604b21d8f78804274185c28cdd4036761
Stefan Negru <stefan@blankdots.com>
2020-08-06 13:26:08 +0300

    bump to version 0.5.0

--- metadata_backend/__init__.py
+++ metadata_backend/__init__.py
@@ -1,5 +1,5 @@
 """Backend for submitting and validating XML Files containing ENA metadata."""
 
 __title__ = "metadata_backend"
-__version__ = VERSION = "0.2.0"
+__version__ = VERSION = "0.5.0"
 __author__ = "CSC Developers"
