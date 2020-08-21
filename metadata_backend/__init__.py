96bd94cce83618bab69400c46a1378f8fec49bbb
Joonatan Mäkinen <joonatan.makinen@csc.fi>
2020-08-21 13:39:44 +0300

    bump version

--- metadata_backend/__init__.py
+++ metadata_backend/__init__.py
@@ -1,5 +1,5 @@
 """Backend for submitting and validating XML Files containing ENA metadata."""
 
 __title__ = "metadata_backend"
-__version__ = VERSION = "0.5.2"
+__version__ = VERSION = "0.5.3"
 __author__ = "CSC Developers"
