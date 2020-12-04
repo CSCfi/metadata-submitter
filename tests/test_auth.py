9bdbbd4dda82588455d7cbcce84b233d69790d33
Joonatan Makinen <joonatan.makinen@csc.fi>
2020-12-04 13:28:04 +0200

    add login test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,3 +12,13 @@ class AccessHandlerTestCase(AioHTTPTestCase):
     async def get_application(self):
         """Retrieve web Application for test."""
         return await init()
+
+    @unittest_run_loop
+    async def test_login_with_default_config_values(self):
+        """Test that login raises 404 because the AUTH_URL env variable is not a proper endpoint."""
+        response = await self.client.get("/aai")
+        self.assertRaises(web.HTTPNotFound)
+        resp_json = await response.json()
+        self.assertEqual(resp_json["instance"], "/authorize")
+        # Also check oidc_state is saved to session storage
+        self.assertIn("oidc_state", self.client.app["Session"])
