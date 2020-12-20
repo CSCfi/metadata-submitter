15a4437f66a3a94324ef98392522ffc194cd89d9
Stefan Negru <stefan@blankdots.com>
2020-12-21 01:12:13 +0200

    auth_referer env var not required

--- metadata_backend/conf/conf.py
+++ metadata_backend/conf/conf.py
@@ -110,5 +110,4 @@ aai_config = {
     "user_info": f'{os.getenv("OIDC_URL", "")}/userinfo',
     "revoke_url": f'{os.getenv("OIDC_URL", "")}/revoke',
     "jwk_server": f'{os.getenv("JWK_URL", "")}',
-    "referer": f'{os.getenv("AUTH_REFERER", "")}',
 }
