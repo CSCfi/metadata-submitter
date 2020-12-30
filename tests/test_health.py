cecebc9765f6619147d98405d55d55c3aa54d3f6
Joonatan Makinen <joonatan.makinen@csc.fi>
2020-12-30 23:29:40 +0200

    mock server info method

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,6 +1,9 @@
 """Test health check endpoint."""
 
+from unittest.mock import patch
+
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+from aiounittest import futurized
 
 from metadata_backend.server import init
 
@@ -13,11 +16,28 @@ class HealthTestCase(AioHTTPTestCase):
         server = await init()
         return server
 
+    async def setUpAsync(self):
+        """Configure values and patches for testing."""
+        self.health_status = {"services": {"database": {"status": "Ok"}}, "status": "Ok"}
+
+        class_motorclient = "metadata_backend.api.health.AsyncIOMotorClient"
+        motorclient_config = {"server_info.side_effect": self.fake_asynciomotorclient_server_info}
+        self.patch_motorclient = patch(class_motorclient, **motorclient_config, spec=True)
+        self.MockedMotorClient = self.patch_motorclient.start()
+
+    async def tearDownAsync(self):
+        """Cleanup mocked stuff."""
+        self.patch_motorclient.stop()
+
+    async def fake_asynciomotorclient_server_info(self):
+        """Fake server info method for a motor client."""
+        return await futurized(True)
+
     @unittest_run_loop
     async def test_health_check_is_down(self):
         """Test that the health check returns a partially down status because a mongo db is not connected."""
         response = await self.client.get("/health")
         self.assertEqual(response.status, 200)
         self.assertEqual(response.content_type, "application/json")
-        health_json = {"status": "Partially down", "services": {"database": {"status": "Down"}}}
-        self.assertEqual(health_json, await response.json())
+        self.assertEqual(self.health_status, await response.json())
+        self.MockedMotorClient().server_info.assert_called_once()
