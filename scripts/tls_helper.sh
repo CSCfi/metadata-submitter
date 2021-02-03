22bfb92d3c946aefc13364d4066763852728a08f
Pontus Freyhult <pontus_github@soua.net>
2021-02-03 14:57:53 +0100

    Generate two service keys/certificates

--- scripts/tls_helper.sh
+++ scripts/tls_helper.sh
@@ -18,3 +18,17 @@ openssl x509 -req -CAcreateserial -sha256 \
               -CAkey config/cakey \
               -in config/cert.csr \
               -out config/cert
+
+openssl req -nodes -new -days 365 -newkey rsa:2048 \
+	    -sha256 -subj '/CN=localhost' \
+	    -keyout config/key2 \
+	    -out config/cert2.csr
+
+openssl x509 -req -CAcreateserial -sha256 \
+              -CA config/cacert \
+              -CAkey config/cakey \
+              -in config/cert2.csr \
+              -out config/cert2
+
+cat config/key config/cert > config/combined
+cat config/key2 config/cert2 > config/combined2
