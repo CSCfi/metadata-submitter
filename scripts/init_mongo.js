ef4256cd6056ffae89166b9a7e0d6b3091317aa6
Evgenia Lyjina <evgenia.lyjina@csc.fi>
2021-12-02 15:32:47 +0000

    Adds indexing on database initialization
    Adds docker-compose configuration for mongo container to run script on
    newly initialized database.
    Adds js script to build database collections and indexes.

---
+++ scripts/init_mongo.js
@@ -0,0 +1,17 @@
+// script to create default database collections and indexes
+// on container start up
+
+db = new Mongo().getDB("default");
+
+db.createCollection('user', { capped: false });
+db.createCollection('folder', { capped: false });
+db.folder.createIndex({ "dateCreated": -1 });
+db.folder.createIndex({ "datePublished": -1 });
+db.folder.createIndex({ "folderId": 1, unique: 1 });
+db.user.createIndex({ "userId": 1, unique: 1 });
+db.folder.createIndex(
+  {
+    text_name: "text",
+  }
+)
+db.folder.getIndexes()
