8df7e61872deea0292d47fb7725eccfd34e58089
Stefan Negru <stefan@blankdots.com>
2022-02-17 21:59:01 +0200

    publish Study and Dataset when publishing folders
    prepare Study and Datasets DOI payload and relationships between them
    
    for now url is omitted till Metax integration is done

--- metadata_backend/helpers/doi.py
+++ metadata_backend/helpers/doi.py
@@ -6,7 +6,7 @@ https://github.com/neicnordic/sda-orchestration/blob/master/sda_orchestrator/uti
 from typing import Dict, Union
 from uuid import uuid4
 
-from aiohttp import web, ClientSession, BasicAuth
+from aiohttp import web, ClientSession, BasicAuth, ClientTimeout
 
 from ..helpers.logger import LOG
 from ..conf import conf
@@ -22,18 +22,20 @@ class DOIHandler:
         self.doi_user = conf.doi_user
         self.doi_key = conf.doi_key
         self.doi_url = f"{conf.datacite_url.rstrip('/')}/{self.doi_prefix}"
+        self.timeout = ClientTimeout(total=2 * 60)  # 2 minutes timeout
+        self.headers = {"Content-Type": "application/vnd.api+json"}
 
     async def create_draft(self, prefix: Union[str, None] = None) -> Dict:
         """Generate random suffix and POST request a draft DOI to DataCite DOI API."""
         suffix = uuid4().hex[:10]
         doi_suffix = f"{prefix}.{suffix[:4]}-{suffix[4:]}" if prefix else f"{suffix[:4]}-{suffix[4:]}"
-        headers = {"Content-Type": "application/json"}
+        # this payload is sufficient to get a draft DOI
         doi_payload = {"data": {"type": "dois", "attributes": {"doi": f"{self.doi_prefix}/{doi_suffix}"}}}
 
         auth = BasicAuth(login=self.doi_user, password=self.doi_key)
-        async with ClientSession(headers=headers, auth=auth) as session:
+        async with ClientSession(headers=self.headers, auth=auth) as session:
             async with session.post(self.doi_api, json=doi_payload) as response:
-                if response.status == 201 or response.status == 200:  # This should only ever be 201
+                if response.status == 201:
                     draft_resp = await response.json()
                     full_doi = draft_resp["data"]["attributes"]["doi"]
                     returned_suffix = draft_resp["data"]["attributes"]["suffix"]
@@ -48,3 +50,24 @@ class DOIHandler:
                     raise web.HTTPBadRequest(reason=reason)  # 400 might not be the correct error for this
 
         return doi_data
+
+    async def set_state(self, doi_payload: dict) -> None:
+        """Set DOI and associated metadata.
+
+        We will only support publish event type, and we expect the data to be
+        prepared for the update.
+        Partial updates are possible.
+
+        :param doi_suffix: DOI to do operations on.
+        :param state: can be publish, register or hide.
+        """
+        auth = BasicAuth(login=self.doi_user, password=self.doi_key)
+        async with ClientSession(headers=self.headers, auth=auth) as session:
+            async with session.put(f"{self.doi_api}/{doi_payload['id']}", json=doi_payload) as response:
+                if response.status == 200:
+                    draft_resp = await response.json()
+                    LOG.debug(f"Datacite doi response: {draft_resp}")
+                else:
+                    reason = f"DOI API set state request failed with code: {response.status}"
+                    LOG.error(reason)
+                    raise web.HTTPBadRequest(reason=reason)  # 400 might not be the correct error for this
