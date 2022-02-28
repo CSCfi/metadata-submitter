0728f9f42d86df9e75bd9fcb8039b30393b5e6e0
Stefan Negru <stefan@blankdots.com>
2022-02-28 18:22:46 +0200

    add to set instead of extending list

--- metadata_backend/api/operators.py
+++ metadata_backend/api/operators.py
@@ -436,7 +436,7 @@ class Operator(BaseOperator):
         # therefore we need to check if the object already exists in database and has metax id
         if schema_type in {"study", "dataset"}:
             read_data = await self.db_service.read(schema_type, accession_id)
-            forbidden_keys.extend(["metaxIdentifier"])
+            forbidden_keys.add("metaxIdentifier")
         if any(i in data for i in forbidden_keys):
             reason = f"Some items (e.g: {', '.join(forbidden_keys)}) cannot be changed."
             LOG.error(reason)
@@ -471,7 +471,7 @@ class Operator(BaseOperator):
                 read_data.get("metaxIdentifier", None)
                 and data["metaxIdentifier"]["identifier"] != read_data["metaxIdentifier"]["identifier"]
             ):
-                forbidden_keys.extend(["metaxIdentifier"])
+                forbidden_keys.add("metaxIdentifier")
         if any(i in data for i in forbidden_keys):
             reason = f"Some items (e.g: {', '.join(forbidden_keys)}) cannot be changed."
             LOG.error(reason)
