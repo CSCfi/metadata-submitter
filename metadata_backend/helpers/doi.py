64409a76ff7454dc447e1c12a3df813ad62ac6e9
Stefan Negru <stefan@blankdots.com>
2022-03-10 08:53:42 +0200

    add option to delete doi on object delete

--- metadata_backend/helpers/doi.py
+++ metadata_backend/helpers/doi.py
@@ -26,7 +26,12 @@ class DOIHandler:
         self.headers = {"Content-Type": "application/vnd.api+json"}
 
     async def create_draft(self, prefix: Union[str, None] = None) -> Dict:
-        """Generate random suffix and POST request a draft DOI to DataCite DOI API."""
+        """Generate random suffix and POST request a draft DOI to DataCite DOI API.
+
+        :param prefix: Custom prefix to add to the DOI e.g. study/dataset
+        :raises: HTTPInternalServerError if we the Datacite DOI draft registration fails
+        :returns: Dictionary with DOI and URL
+        """
         suffix = uuid4().hex[:10]
         doi_suffix = f"{prefix}.{suffix[:4]}-{suffix[4:]}" if prefix else f"{suffix[:4]}-{suffix[4:]}"
         # this payload is sufficient to get a draft DOI
@@ -47,27 +52,48 @@ class DOIHandler:
                 else:
                     reason = f"DOI API draft creation request failed with code: {response.status}"
                     LOG.error(reason)
-                    raise web.HTTPBadRequest(reason=reason)  # 400 might not be the correct error for this
+                    raise web.HTTPInternalServerError(reason=reason)
 
         return doi_data
 
-    async def set_state(self, doi_payload: dict) -> None:
+    async def set_state(self, doi_payload: Dict) -> None:
         """Set DOI and associated metadata.
 
         We will only support publish event type, and we expect the data to be
         prepared for the update.
         Partial updates are possible.
 
-        :param doi_suffix: DOI to do operations on.
-        :param state: can be publish, register or hide.
+        :param doi_payload: Dictionary with payload to send to Datacite
+        :raises: HTTPInternalServerError if we the Datacite DOI update fails
+        :returns: None
         """
         auth = BasicAuth(login=self.doi_user, password=self.doi_key)
         async with ClientSession(headers=self.headers, auth=auth) as session:
             async with session.put(f"{self.doi_api}/{doi_payload['id']}", json=doi_payload) as response:
                 if response.status == 200:
-                    draft_resp = await response.json()
-                    LOG.debug(f"Datacite doi response: {draft_resp}")
+                    _resp = await response.json()
+                    LOG.info(f"Datacite doi {doi_payload['id']} updated ")
+                    LOG.debug(f"Datacite doi {doi_payload['id']} updated, response: {_resp}")
                 else:
                     reason = f"DOI API set state request failed with code: {response.status}"
                     LOG.error(reason)
-                    raise web.HTTPBadRequest(reason=reason)  # 400 might not be the correct error for this
+                    raise web.HTTPInternalServerError(reason=reason)
+
+    async def delete(self, doi: str) -> None:
+        """Delete DOI and associated metadata.
+
+        Datacite only support deleting draft DOIs.
+
+        :param doi: identifier to be utilized for deleting draft DOI
+        :raises: HTTPInternalServerError if we the Datacite draft DOI delete fails
+        :returns: None
+        """
+        auth = BasicAuth(login=self.doi_user, password=self.doi_key)
+        async with ClientSession(headers=self.headers, auth=auth) as session:
+            async with session.delete(f"{self.doi_api}/{doi}") as response:
+                if response.status == 204:
+                    LOG.info(f"Datacite doi {doi} deleted.")
+                else:
+                    reason = f"DOI API delete request failed with code: {response.status}"
+                    LOG.error(reason)
+                    raise web.HTTPInternalServerError(reason=reason)
