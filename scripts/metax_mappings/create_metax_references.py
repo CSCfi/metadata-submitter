a29a3c6d93cd6e22227d16742def3f8063e86a9d
Joonatan Mäkinen <joonatan.makinen@csc.fi>
2022-08-17 12:48:30 +0300

    Update metax mapping methods for subjects
    Also metax reference script is updated to fetch
    metax reference fields of subjects to map

--- scripts/metax_mappings/create_metax_references.py
+++ scripts/metax_mappings/create_metax_references.py
@@ -12,7 +12,7 @@ log.setLevel(logging.DEBUG)
 # Resources are fetched from external URI and parsed for submitter use in metax_mapper class
 
 # parsing codes for mapping identifier types
-metax_ref = {"identifier_types": {}, "languages": {}}
+metax_ref = {"identifier_types": {}, "languages": {}, "fields_of_science": {}}
 codes_url = (
     "https://koodistot.suomi.fi/codelist-api/api/v1/coderegistries/fairdata/codeschemes/identifier_type/"
     "?format=json&embedCodes=true&downloadFile=true&pretty"
@@ -29,18 +29,18 @@ for code in codes_json:
 ISO_langs = {}
 
 # parsing ISO 639-1, ISO 639-2 and ISO 639-3 language format
-lexovo_urls = [
+lexvo_urls = [
     "http://www.lexvo.org/resources/lexvo-iso639-3.tsv",
     "http://www.lexvo.org/resources/lexvo-iso639-2.tsv",
     "http://www.lexvo.org/resources/lexvo-iso639-1.tsv",
 ]
-for url in lexovo_urls:
-    lexovo_text = requests.get(url).text
-    lexovo_list = [lang.split("\t") for lang in lexovo_text.split("\n")]
-    lexovo_dict = {lang[0]: lang[1] for lang in lexovo_list[:-1]}
+for url in lexvo_urls:
+    lexvo_text = requests.get(url).text
+    lexvo_list = [lang.split("\t") for lang in lexvo_text.split("\n")]
+    lexvo_dict = {lang[0]: lang[1] for lang in lexvo_list[:-1]}
 
     # The keys are language codes of 2 and 3 chars, and values are URLs
-    ISO_langs.update(lexovo_dict)
+    ISO_langs.update(lexvo_dict)
 
 
 # parsing languages and their sub-tags
@@ -68,6 +68,16 @@ for k, v in lang_dict.items():
         no_matches += 1
         pass
 log.error(f"{n_matches} languages matched, and {no_matches} didn't")
+
+
+# parsing codes and identifiers for mapping fields of science
+fos_codes_url = "https://metax.demo.fairdata.fi/es/reference_data/field_of_science/_search?size=100"
+fos_codes_json = requests.get(fos_codes_url).json()["hits"]["hits"]
+for fos in fos_codes_json:
+    metax_ref["fields_of_science"][fos["_source"]["code"]] = {
+        "uri": fos["_source"]["uri"],
+        "label": fos["_source"]["label"],
+    }
 
 # print to console, so the script caller should output it to the correct file
 print(json.dumps(metax_ref))
