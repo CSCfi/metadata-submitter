c74741f95bc6d0c63d178092c6fabaf3c031130a
Felipe Morato <felipe.morato@csc.fi>
2022-09-06 16:46:05 +0300

    Fix bp schema test, and fix couple of related bugs

--- tests/integration/run_tests.py
+++ tests/integration/run_tests.py
@@ -22,7 +22,7 @@ from .conf import (
     base_url,
     datacite_url,
     drafts_url,
-    metax_url,
+    metax_api,
     objects_url,
     publish_url,
     schemas_url,
@@ -584,7 +584,7 @@ async def delete_objects_metax_id(client_logged_in, database, collection, access
     except Exception as e:
         LOG.error(f"Object update failed due to {str(e)}")
     try:
-        await client_logged_in.delete(f"{metax_url}/{metax_id}")
+        await client_logged_in.delete(f"{metax_api}/{metax_id}")
     except Exception as e:
         LOG.error(f"Object deletion from mocked Metax failed due to {str(e)}")
 
@@ -1074,7 +1074,7 @@ class TestMetax:
                 except KeyError:
                     assert False, "Metax ID was not in response data"
             object.append(metax_id)
-            async with client_logged_in.get(f"{metax_url}/{metax_id}", auth=auth) as metax_resp:
+            async with client_logged_in.get(f"{metax_api}/{metax_id}", auth=auth) as metax_resp:
                 assert metax_resp.status == 200, f"HTTP Status code error, got {metax_resp.status}"
                 metax_res = await metax_resp.json()
                 assert (
@@ -1086,7 +1086,7 @@ class TestMetax:
             await put_object_xml(client_logged_in, schema, accession_id, filename)
 
         for _, _, metax_id in ids:
-            async with client_logged_in.get(f"{metax_url}/{metax_id}", auth=auth) as metax_resp:
+            async with client_logged_in.get(f"{metax_api}/{metax_id}", auth=auth) as metax_resp:
                 assert metax_resp.status == 200, f"HTTP Status code error, got {metax_resp.status}"
                 metax_res = await metax_resp.json()
                 assert (
@@ -1098,7 +1098,7 @@ class TestMetax:
             await delete_object(client_logged_in, schema, accession_id)
 
         for _, _, metax_id in ids:
-            async with client_logged_in.get(f"{metax_url}/{metax_id}", auth=auth) as metax_resp:
+            async with client_logged_in.get(f"{metax_api}/{metax_id}", auth=auth) as metax_resp:
                 assert (
                     metax_resp.status == 404
                 ), f"HTTP Status code error - expected 404 Not Found, got {metax_resp.status}"
@@ -1129,7 +1129,7 @@ class TestMetax:
                 except KeyError:
                     assert False, "Metax ID was not in response data"
             object.append(metax_id)
-            async with client_logged_in.get(f"{metax_url}/{metax_id}", auth=auth) as metax_resp:
+            async with client_logged_in.get(f"{metax_api}/{metax_id}", auth=auth) as metax_resp:
                 assert metax_resp.status == 200, f"HTTP Status code error, got {metax_resp.status}"
                 metax_res = await metax_resp.json()
                 assert (
@@ -1199,7 +1199,7 @@ class TestMetax:
                 res = await resp.json()
                 assert res["metaxIdentifier"] == metax_id
 
-            async with client_logged_in.get(f"{metax_url}/{metax_id}") as metax_resp:
+            async with client_logged_in.get(f"{metax_api}/{metax_id}") as metax_resp:
                 assert metax_resp.status == 200, f"HTTP Status code error, got {metax_resp.status}"
                 metax_res = await metax_resp.json()
                 assert metax_res["state"] == "published", f"{schema}  {metax_res}"
@@ -1245,7 +1245,7 @@ class TestMetax:
         for _, _, metax_id in objects:
             # delete of published metax datasets is possible only from mocked metax for testing purpose
             # Metax service does not allow deleting published datasets
-            await client_logged_in.delete(f"{metax_url}/{metax_id}", params={"test": "true"})
+            await client_logged_in.delete(f"{metax_api}/{metax_id}", params={"test": "true"})
 
 
 class TestMetaxMissingId:
@@ -1430,53 +1430,53 @@ class TestSubmissionOperations:
             LOG.debug(f"Checking that JSON object {accession_id} was deleted")
             assert resp.status == 404, f"HTTP Status code error, got {resp.status}"
 
-    # async def test_bpdataset_gets_doi(self, client_logged_in, project_id, submission_id):
-    #     """Test bp dataset has doi generated.
-    #
-    #     :param client_logged_in: HTTP client in which request call is made
-    #     :param project_id: id of the project the submission belongs to
-    #     """
-    #     # Create new submission
-    #     # submission_data = {
-    #     #     "name": "Test bpdataset submission",
-    #     #     "description": "Test that DOI is generated for bp dataset",
-    #     #     "projectId": project_id,
-    #     # }
-    #     # submission_id = await post_submission(client_logged_in, submission_data)
-    #
-    #     # Submit study, bpdataset
-    #     study = await post_object_json(client_logged_in, "study", submission_id, "SRP000539.json")
-    #     study = await get_object(client_logged_in, "study", study)
-    #
-    #     bpdataset = await post_object(client_logged_in, "bpdataset", submission_id, "template_dataset.xml")
-    #     bpdataset = await get_object(client_logged_in, "bpdataset", bpdataset[0])
-    #     assert bpdataset["doi"] is not None
-    #
-    #     # Add DOI for publishing the submission
-    #     doi_data_raw = await create_request_json_data("doi", "test_doi.json")
-    #     await put_submission_doi(client_logged_in, submission_id, doi_data_raw)
-    #
-    #     dac_data = await create_request_json_data("dac", "dac_rems.json")
-    #     await put_submission_dac(client_logged_in, submission_id, dac_data)
-    #
-    #     submission_id = await publish_submission(client_logged_in, submission_id)
-    #
-    #     # check that datacite has references between datasets and study
-    #     async with client_logged_in.get(f"{datacite_url}/{bpdataset['doi']}") as datacite_resp:
-    #         assert datacite_resp.status == 200, f"HTTP Status code error, got {datacite_resp.status}"
-    #         datacite_res = await datacite_resp.json()
-    #         bpdataset = datacite_res["data"]
-    #
-    #     async with client_logged_in.get(f"{datacite_url}/{study['doi']}") as datacite_resp:
-    #         assert datacite_resp.status == 200, f"HTTP Status code error, got {datacite_resp.status}"
-    #         datacite_res = await datacite_resp.json()
-    #         study = datacite_res["data"]
-    #     assert bpdataset["data"]["attributes"]["relatedIdentifiers"][0]["relatedIdentifier"] == study["id"]
-    #     for id in study["data"]["attributes"]["relatedIdentifiers"]:
-    #         assert study["data"]["attributes"]["relatedIdentifiers"][0]["relatedIdentifier"] == bpdataset["id"]
-    #
-    #     # Delete submission
-    #     await delete_submission_publish(client_logged_in, submission_id)
+    async def test_bpdataset_gets_doi(self, client_logged_in, project_id):
+        """Test bp dataset has doi generated.
+
+        :param client_logged_in: HTTP client in which request call is made
+        :param project_id: id of the project the submission belongs to
+        """
+        # Create new submission
+        submission_data = {
+            "name": "Test bpdataset submission",
+            "description": "Test that DOI is generated for bp dataset",
+            "projectId": project_id,
+        }
+        submission_id = await post_submission(client_logged_in, submission_data)
+
+        # Submit study, bpdataset
+        study = await post_object_json(client_logged_in, "study", submission_id, "SRP000539.json")
+        study = await get_object(client_logged_in, "study", study)
+
+        bpdataset = await post_object(client_logged_in, "bpdataset", submission_id, "template_dataset.xml")
+        bpdataset = await get_object(client_logged_in, "bpdataset", bpdataset[0])
+        assert bpdataset["doi"] is not None
+
+        # Add DOI for publishing the submission
+        doi_data_raw = await create_request_json_data("doi", "test_doi.json")
+        await put_submission_doi(client_logged_in, submission_id, doi_data_raw)
+
+        dac_data = await create_request_json_data("dac", "dac_rems.json")
+        await put_submission_dac(client_logged_in, submission_id, dac_data)
+
+        submission_id = await publish_submission(client_logged_in, submission_id)
+
+        # check that datacite has references between datasets and study
+        async with client_logged_in.get(f"{datacite_url}/{bpdataset['doi']}") as datacite_resp:
+            assert datacite_resp.status == 200, f"HTTP Status code error, got {datacite_resp.status}"
+            datacite_res = await datacite_resp.json()
+            bpdataset = datacite_res
+
+        async with client_logged_in.get(f"{datacite_url}/{study['doi']}") as datacite_resp:
+            assert datacite_resp.status == 200, f"HTTP Status code error, got {datacite_resp.status}"
+            datacite_res = await datacite_resp.json()
+            study = datacite_res
+        assert bpdataset["data"]["attributes"]["relatedIdentifiers"][0]["relatedIdentifier"] == study["id"]
+        for _ in study["data"]["attributes"]["relatedIdentifiers"]:
+            assert study["data"]["attributes"]["relatedIdentifiers"][0]["relatedIdentifier"] == bpdataset["id"]
+
+        # Delete submission
+        await delete_submission_publish(client_logged_in, submission_id)
 
     async def test_crud_submissions_works_no_publish(self, client_logged_in, project_id):
         """Test submissions REST API POST, GET, PATCH, PUBLISH and DELETE reqs.
