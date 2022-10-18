be30b4c673f65781f4a14cf172a7fafbd438d52e
Felipe Morato <felipe.morato@csc.fi>
2022-10-18 15:24:58 +0300

    Remove deduplication code from pre-commit sort
    Keep it as a separate script that can be run manually.

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -4,17 +4,3 @@ file="docs/dictionary/wordlist.txt"
 
 # Sorts dictionary, converts to lower case, and remove duplicates
 sort "${file}" | tr "[:upper:]" "[:lower:]" | sort -u -o "${file}"
-
-# Remove unused words
-# Uses `ripgrep` to look for each word in the repository, and removes them with `sed` when no match is found
-if ! command -v rg > /dev/null 2>&1; then
-    echo "ripgrep not installed, skipping removing unused words"
-    exit 0
-fi
-
-while read -r line; do
-  if ! output="$(rg --hidden -g '!docs/dictionary' -i "${line}" .)" ; then
-    echo $line
-    sed -i "/${line}/d" "$file"
-  fi
-done <"${file}"
