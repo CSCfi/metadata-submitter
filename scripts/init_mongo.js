e33b913342f9412decf97131b64107d8c1b50630
Stefan Negru <stefan@blankdots.com>
2022-10-24 01:30:11 +0300

    adjust file collection to standard notation
    - project should be projectId to be consinstent with other collections
    - submission index is not required any more

--- scripts/init_mongo.js
+++ scripts/init_mongo.js
@@ -13,7 +13,6 @@ db.submission.createIndex({ "submissionId": 1, unique: 1 });
 db.user.createIndex({ "userId": 1, unique: 1 });
 db.file.createIndex({ "accessionId": 1, unique: 1 });
 db.file.createIndex({ "path": 1 });
-db.file.createIndex({ "submissions": 1 });
 db.submission.createIndex(
   {
     text_name: "text",
