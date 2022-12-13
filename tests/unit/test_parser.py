c8ffec83f9c66a265822c2cd399152c814dec876
Joonatan Mäkinen <joonatan.makinen@csc.fi>
2022-12-13 17:42:35 +0200

    Add BP observation xml schema and test file

--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -173,6 +173,17 @@ class ParserTestCase(unittest.TestCase):
         self.assertEqual("sample_preparation", bp_sample_json[6]["block"]["attributes"][0]["tag"])
         self.assertEqual("something", bp_sample_json[9]["slide"]["stainingInformation"]["refname"])
 
+    def test_observation_is_parsed(self):
+        """Test that BP observation is parsed correctly.
+
+        Tests for some values that converted JSON should have.
+        """
+        observation_xml = self.load_file_to_text("observation", "observations.xml")
+        observation_json = self.xml_parser.parse("observation", observation_xml)
+        self.assertEqual("Observation_gHJhZLyAMP", observation_json["alias"])
+        self.assertEqual("Case_GMGVsazraj", observation_json["observedOn"]["case"]["refname"])
+        self.assertEqual("Diagnose", observation_json["statement"]["codedAttributesSet"]["codedAttribute"]["tag"])
+
     def test_error_raised_when_schema_not_found(self):
         """Test 400 is returned when schema type is invalid."""
         with self.assertRaises(web.HTTPBadRequest):
