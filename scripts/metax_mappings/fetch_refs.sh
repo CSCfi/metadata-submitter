dc721f80b998cc393501d51687f1e8f6645a6fc4
Hang Le <lhang@csc.fi>
2024-07-01 15:18:13 +0300

    Generate script for fundingReferences

--- scripts/metax_mappings/fetch_refs.sh
+++ scripts/metax_mappings/fetch_refs.sh
@@ -11,3 +11,4 @@ mkdir -p "${MAPPING_FILES_ROOT}"
 python3 "${SCRIPT_ROOT}"/create_metax_references.py get_identifier_types> "${MAPPING_FILES_ROOT}"/identifier_types.json
 python3 "${SCRIPT_ROOT}"/create_metax_references.py get_languages> "${MAPPING_FILES_ROOT}"/languages.json
 python3 "${SCRIPT_ROOT}"/create_metax_references.py get_fields_of_science> "${MAPPING_FILES_ROOT}"/fields_of_science.json
+python3 "${SCRIPT_ROOT}"/create_metax_references.py get_funding_references> "${MAPPING_FILES_ROOT}"/funding_references.json
