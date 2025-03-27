c1091498bd15df4d4c3d7d2194626df431d413f8
Monika Radaviciute <mradavic@csc.fi>
2025-03-27 14:17:27 +0200

    Move DOI integration tests to one file

--- tests/unit/test_doi.py
+++ tests/unit/test_doi.py
@@ -1,4 +1,4 @@
-"""Test the Datacite DOI registering tool."""
+"""Test DOI registering tools."""
 
 import unittest
 from unittest.mock import AsyncMock
@@ -6,6 +6,7 @@ from unittest.mock import AsyncMock
 from aiohttp import web
 
 from metadata_backend.services.datacite_service_handler import DataciteServiceHandler
+from metadata_backend.services.pid_ms_handler import PIDServiceHandler
 
 
 class DataciteTestCase(unittest.IsolatedAsyncioTestCase):
@@ -42,3 +43,26 @@ class DataciteTestCase(unittest.IsolatedAsyncioTestCase):
         doi = await self.datacite.create_draft_doi_datacite("dataset")
         assert self.datacite._request.assert_called_once
         self.assertEqual(doi, example_doi)
+
+
+class PIDTestCase(unittest.IsolatedAsyncioTestCase):
+    """PID DOI registering class test case."""
+
+    def setUp(self):
+        """Set class for tests."""
+        self.pid = PIDServiceHandler()
+
+    async def test_400_is_raised(self):
+        """Test 400 is raised when request to PID supposedly fails."""
+        self.pid._request = AsyncMock(side_effect=web.HTTPBadRequest)
+        with self.assertRaises(web.HTTPBadRequest):
+            await self.pid.create_draft_doi_pid()
+        assert self.pid._request.assert_called_once
+
+    async def test_create_doi_draft_works(self):
+        """Test draft DOI is returned correctly."""
+        example_doi = "10.80869/sd-2108ec42-6ae9-39c0-9941-2ef802ff5b7f"
+        self.pid._request = AsyncMock(return_value=example_doi)
+        draft_doi = await self.pid.create_draft_doi_pid()
+        assert self.pid._request.assert_called_once
+        self.assertEqual(draft_doi, example_doi)
