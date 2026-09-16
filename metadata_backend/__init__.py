258413035887706442a94071f6a31b017c21abc1
releases-token <group_183_bot_7e9e6935eae78180381f92890bc29804@noreply.gitlab.ci.csc.fi>
2026-09-16 13:41:01 +0000

    Chore: automatically update versions [ci skip]

--- metadata_backend/__init__.py
+++ metadata_backend/__init__.py
@@ -1,5 +1,5 @@
 """Backend for submitting and validating XML Files containing ENA metadata."""
 
 __title__ = "metadata_backend"
-__version__ = "2026.9.0"
+__version__ = "2026.9.1"
 __author__ = "CSC Developers"
